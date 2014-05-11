15f53960e3800e6bf27523eec73626e604ff4d3c
Silvina Ganas <silvina.ganas@gmail.com>
2014-05-11 15:48:45 -0300

    Hilos de I/O en Kernel y datos de config

---
+++ kernel/kernel.h
@@ -0,0 +1,32 @@
+/*
+ * kernel.h
+ *
+ *  Created on: 11/05/2014
+ *      Author: utnso
+ */
+
+#ifndef KERNEL_H_
+#define KERNEL_H_
+#include <semaphore.h>
+
+typedef struct
+{
+	int puertoprog;
+	int puertocpu;
+	int quantum;
+	int retardo;
+	int multiprogramacion;
+	char** entradasalidaid;
+	char** entradasalidaret;
+	char** semaforosid;
+	char** semaforosvalor;
+}t_kernel;
+
+typedef struct
+{
+	char *id;
+	int retardo;
+	sem_t *semaforo_IO;
+}t_entradasalida;
+
+#endif /* KERNEL_H_ */
