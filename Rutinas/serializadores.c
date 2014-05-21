e4cca20a7e2592c7e29ff9bafe95e2709cf669ce
Silvina Ganas <silvina.ganas@gmail.com>
2014-05-21 10:19:43 -0300

    Agrego estructura para el pasaje de mensajes umv-kernel, y funciones de serializacion

--- Rutinas/serializadores.c
+++ Rutinas/serializadores.c
@@ -20,21 +20,18 @@ char* serializarSolicitudSegmento(t_crearSegmentoUMV *segmento){
 
 	return stream;
 }
-char* serializarEnvioBytes(t_envioBytesUMV *envioBytes){
-	char *stream = malloc(sizeof(t_envioBytesUMV));
-	int size =0, offset =0;
-	size = sizeof(int);
-	memcpy(stream + offset, &envioBytes->base, size);
-	offset +=size;
-	size = sizeof(int);
-	memcpy(stream + offset, &envioBytes->offset, size);
-	offset +=size;
-	size = sizeof(int);
-	memcpy(stream + offset, &envioBytes->size, size);
-	offset +=size;
-	size = strlen(envioBytes->buffer) +1;
-	memcpy(stream+offset,envioBytes->buffer,size);
-	return stream;
+
+t_crearSegmentoUMV *deserializarSolicitudSegmento(char *solicitud){
+	int offset = 0, tmp_size = 0;
+	t_crearSegmentoUMV * segmento = malloc(sizeof(t_crearSegmentoUMV));
+	tmp_size = sizeof(t_puntero);
+	memcpy(&segmento->programid,solicitud+offset,tmp_size);
+
+	offset += tmp_size;
+	tmp_size = sizeof(t_puntero);
+	memcpy(&segmento->size,solicitud+offset,tmp_size);
+
+	return segmento;
 }
 
 char* serializarSolicitudLectura(t_solicitudLectura* solicitud){
@@ -111,149 +108,4 @@ t_solicitudEscritura* desserializarSolicitudEscritura(char* solicitud){
 
 	return solic;
 }
-
-
-char* serializarPresentacionPersonaje(t_presentacionPersonaje * pers){
-	char *stream = malloc(strlen(pers->nomNivel)+1 + strlen(pers->nomPersonaje)+1);
-	int size=0, offset=0;
-	memcpy(stream, pers->nomNivel, size = strlen(pers->nomNivel)+1);
-	offset += size;
-	size = strlen(pers->nomPersonaje)+1;
-	memcpy (stream + offset, pers->nomPersonaje, size);
-	return stream;
-}
-
-t_presentacionPersonaje * desserializarPresentPersonaje (char* pers){
-	int offset = 0, tmp_size = 0;
-	t_presentacionPersonaje * present ;
-	present = malloc(sizeof(t_presentacionPersonaje));
-
-	for (tmp_size=1 ; (pers+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->nomNivel = malloc (tmp_size);
-	memcpy(present->nomNivel, pers+offset, tmp_size);
-
-	offset += tmp_size;
-
-	for (tmp_size=1 ; (pers+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->nomPersonaje = malloc (tmp_size);
-	memcpy(present->nomPersonaje, pers+offset, tmp_size);
-
-	return present;
-
-}
-
-
-char* serializarPresNivel(t_presentacionNivel * pres){
-	char *stream = malloc(strlen(pres->nomNivel)+1 + strlen(pres->algoritmo)+1 + sizeof(int16_t)*2);
-	int size=0, offset=0;
-	memcpy(stream, pres->nomNivel, size = strlen(pres->nomNivel)+1);
-	offset += size;
-	size = strlen(pres->algoritmo)+1;
-	memcpy (stream + offset, pres->algoritmo, size);
-	offset += size;
-	size = sizeof(int16_t);
-	memcpy (stream + offset, &pres->quantum, size);
-	offset += size;
-	size = sizeof(int16_t);
-	memcpy (stream + offset, &pres->retardo, size);
-
-	return stream;
-}
-
-
-t_presentacionNivel * desserializarPresNivel (char* pres){
-	int offset = 0, tmp_size = 0;
-	t_presentacionNivel * present ;
-	present = malloc(sizeof(t_presentacionNivel));
-
-	for (tmp_size=1 ; (pres+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->nomNivel = malloc (tmp_size);
-	memcpy(present->nomNivel, pres+offset, tmp_size);
-
-	offset += tmp_size;
-
-	for (tmp_size=1 ; (pres+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->algoritmo = malloc (tmp_size);
-	memcpy(present->algoritmo, pres+offset, tmp_size);
-
-	offset += tmp_size;
-	tmp_size = sizeof(int16_t);
-	memcpy(&present->quantum,pres+offset,tmp_size);
-
-	offset += tmp_size;
-	tmp_size = sizeof(int16_t);
-	memcpy(&present->retardo,pres+offset,tmp_size);
-
-
-	return present;
-}
-
-
-
-char* serializarNivelConRecursos(t_nivelConRecursos * pres){
-	char *stream = malloc(strlen(pres->nomNivel)+1 + strlen(pres->recursos)+1);
-	int size=0, offset=0;
-	memcpy(stream, pres->nomNivel, size = strlen(pres->nomNivel)+1);
-	offset += size;
-	size = strlen(pres->recursos)+1;
-	memcpy (stream + offset, pres->recursos, size);
-	return stream;
-}
-
-
-t_nivelConRecursos * desserializarNivelConRecursos (char* pres){
-	int offset = 0, tmp_size = 0;
-	t_nivelConRecursos * present ;
-	present = malloc(sizeof(t_nivelConRecursos));
-
-	for (tmp_size=1 ; (pres+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->nomNivel = malloc (tmp_size);
-	memcpy(present->nomNivel, pres+offset, tmp_size);
-
-	offset += tmp_size;
-
-	for (tmp_size=1 ; (pres+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->recursos = malloc (tmp_size);
-	memcpy(present->recursos, pres+offset, tmp_size);
-
-
-	return present;
-}
-
-
-
-
-
-// PARA deadlcok
-char* serializarNivelConCulpables(t_nivelConCulpables * pres){
-	char *stream = malloc(strlen(pres->nomNivel)+1 + strlen(pres->personajes)+1);
-	int size=0, offset=0;
-	memcpy(stream, pres->nomNivel, size = strlen(pres->nomNivel)+1);
-	offset += size;
-	size = strlen(pres->personajes)+1;
-	memcpy (stream + offset, pres->personajes, size);
-	return stream;
-}
-
-
-t_nivelConCulpables * desserializarNivelConCulpables (char* pres){
-	int offset = 0, tmp_size = 0;
-	t_nivelConCulpables * present ;
-	present = malloc(sizeof(t_nivelConCulpables));
-
-	for (tmp_size=1 ; (pres+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->nomNivel = malloc (tmp_size);
-	memcpy(present->nomNivel, pres+offset, tmp_size);
-
-	offset += tmp_size;
-
-	for (tmp_size=1 ; (pres+offset) [tmp_size-1] != '\0'; tmp_size++);
-	present->personajes = malloc (tmp_size);
-	memcpy(present->personajes, pres+offset, tmp_size);
-
-
-	return present;
-}
-
-
 
--- Rutinas/serializadores.h
+++ Rutinas/serializadores.h
@@ -14,40 +14,12 @@
 
 typedef u_int32_t t_puntero;
 
-typedef struct{
-	int base;
-	int offset;
-	int size;
-	char* buffer;
-}__attribute__((packed)) t_envioBytesUMV;
 
 typedef struct{
-	int programid;
-	int size;
+	t_puntero programid;
+	t_puntero size;
 }__attribute__((packed)) t_crearSegmentoUMV;
 
-typedef struct{
-	char* nomNivel;
-	char* nomPersonaje;
-	char* simbolo;
-}__attribute__((packed)) t_presentacionPersonaje;
-
-typedef struct{
-	char *nomNivel;
-	int16_t *retardo;
-	int16_t *quantum;
-	char *algoritmo;
-}__attribute__((packed)) t_presentacionNivel;
-
-typedef struct{
-	char *nomNivel;
-	char *recursos;
-}__attribute__((packed)) t_nivelConRecursos;
-
-typedef struct{
-	char *nomNivel;
-	char *personajes;
-}__attribute__((packed)) t_nivelConCulpables;
 
 typedef struct{
 	t_puntero* base;
@@ -70,19 +42,5 @@ char* serializarSolicitudEscritura(t_solicitudEscritura* solicitud);
 t_solicitudEscritura* desserializarSolicitudEscritura(char* solicitud);
 
 char* serializarSolicitudSegmento(t_crearSegmentoUMV *segmento);
-char* serializarEnvioBytes(t_envioBytesUMV *envioBytes);
-char* serializarPresentacionPersonaje(t_presentacionPersonaje * pers);
-t_presentacionPersonaje * desserializarPresentPersonaje (char* pers);
-
-char* serializarPresNivel(t_presentacionNivel * pres);
-t_presentacionNivel * desserializarPresNivel (char* pres);
-
-char* serializarNivelConRecursos(t_nivelConRecursos * pres);
-t_nivelConRecursos * desserializarNivelConRecursos(char* pres);
-
-char* serializarNivelConCulpables(t_nivelConCulpables * pres);
-t_nivelConCulpables * desserializarNivelConCulpables(char* pres);
-
-//char* serializarSolicitudSegmentosPCB(t_solicitudSegmentosPCB * solicitudSegmentosPCB);
 
 #endif /* SERIALIZADORES_H_ */
